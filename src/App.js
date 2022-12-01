998f401e6fedd00b4ce18bedb444831d2d5366ad
alessiocremo <alessiocremo@DESKTOP-L55JODL.localdomain>
2022-12-01 12:23:33 +0200

    1:04:30

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import TopBar from "./components/topbar/TopBar.jsx";
 import Single from "./pages/single/Single.jsx";
+import Write from "./pages/write/Write.jsx";
 
 function App() {
   return (
     <>
       <TopBar/>
-      <Single/>
+      <Write/>
     </>
   );
 }
