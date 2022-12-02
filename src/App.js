09d0ed1073d2fc3079980bfd99a10ca0273484fa
alessiocremo <alessiocremo@DESKTOP-L55JODL.localdomain>
2022-12-02 20:48:15 +0200

    fixed links, fonts, centerings

--- src/App.js
+++ src/App.js
@@ -21,15 +21,13 @@ function App() {
     <BrowserRouter>
       <TopBar/>
       <Routes>
-        <Route path='/' element ={<Home/>}/>
+        <Route exact path='/' element ={<Home/>}/>
         <Route path='/register' element ={user ? <Home/> : <Register/>}/>
         <Route path='/login' element ={user ? <Home/> :<Login/>}/>
         <Route path='/write' element ={user ? <Write/> : <Login/>}/>
         <Route path='/settings' element ={user ? <Settings/>: <Login/>}/>
         <Route path='/post/:postId' element ={<Single/>}/>
-        <Route path='/post/:postId' element ={<Single/>}/>
       </Routes>
-      <Register/>
     </BrowserRouter>
   );
 }
