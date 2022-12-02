9ca407f1bcd26a744b05aa31b0b0463ea2c618f4
alessiocremo <alessiocremo@DESKTOP-L55JODL.localdomain>
2022-12-02 12:42:12 +0200

    login page completed

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -5,14 +5,14 @@ export default function Login() {
     <div className="login">
         <span className="loginTitle">Login</span>
         <form action="" className="loginForm">
-            <label>Email</label>
+            <label><span>Email</span></label>
             <input type="text" className="loginInput" placeholder='Enter your email...'/>
-            <label>Password</label>
+            <label><span>Password</span></label>
             <input type="password" className='loginInput' placeholder='Enter your password...'/>
             <button className="loginButton">Login</button>
         </form>
         <div className="register">
-                <div className='notAMemberText'>Not a mem</div>
+                <div className='notAMemberText'>Not a member?</div>
                 <button className="loginRegisterButton">Register</button>
         </div>
     </div>
